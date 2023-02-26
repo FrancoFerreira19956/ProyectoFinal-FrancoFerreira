122be06129641d5b7a4df8a6c8dbeaba812e3dcf
FrancoFerreira19956 <franco1995ferreira@gmail.com>
2023-02-26 16:04:07 -0300

    proyecto final CV

---
+++ botones.js
@@ -0,0 +1,26 @@
+document.getElementById('boton').addEventListener('click' = function () {
+
+            console.log("TommyAlvarez@ejemplo.com");
+            document.getElementById("boton").innerHTML = "TommyAlvarez@ejemplo.com";
+
+
+})
+
+document.getElementById('boton2').addEventListener('click' = function () {
+
+    console.log("(341) 795-4320");
+    document.getElementById("boton2").innerHTML = "(341) 795-4320";
+
+
+})
+
+document.getElementById('boton3').addEventListener('click' = function () {
+
+    console.log("5405 Stevens Creek Blvd");
+    document.getElementById("boton3").innerHTML = "5405 Stevens Creek Blvd";
+
+
+})
+
+
+//no me deja conectar java con html, me tira un error (failed to load resource) y no pude resolverlo
